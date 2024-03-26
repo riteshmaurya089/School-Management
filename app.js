453aeedc2be8214791831057f6d444b275759687
RITESH <ayanmaurya89@gmail.com>
2024-03-26 09:35:29 +0530

    update1

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB Connection Error:', err));
